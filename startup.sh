5f240a3237168c59edce71080cf11f1831c33c92
peterrobinson <peter.robinson@usask.ca>
2024-11-06 14:28:57 +0000

    fixed for git

--- startup.sh
+++ startup.sh
@@ -4,6 +4,6 @@ BASEDIR=$(pwd)
 java -classpath collatex/lib/\* -Dapp.name="collatex-server" -Dapp.repo="collatex/lib" -Dapp.home="collatex" -Dbasedir="collatex" eu.interedition.collatex.http.Server &
 COLLATEXPID=$!
 
-python python/bottle_server.py $BASEDIR
+python2 python/bottle_server.py $BASEDIR
 
 kill $COLLATEXPID
